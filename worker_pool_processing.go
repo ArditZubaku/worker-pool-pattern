770f781fcfca7b74b82389b55faf1e8b6698b2d6
ArditZubaku <zubaku92@gmail.com>
2025-12-05 15:51:44 +0100

    Implement CSV processing with worker pool for concurrent job handling

---
+++ worker_pool_processing.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/csv"
+	"log/slog"
+	"os"
+	"sync"
+)
+
+var (
+	jobs = make(chan *Job, 100)
+	wg   sync.WaitGroup
+)
+
+func processCSVWithWorkerPool() {
+	file, err := os.Open("testdata/data.csv")
+	if err != nil {
+		slog.Error("Error opening file", slog.String("err", err.Error()))
+		return
+	}
+	defer closeOrLog(file, "")
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		slog.Error("Error reading CSV", slog.String("err", err.Error()))
+	}
+
+	numOfWorkers := 5
+	createWorkerPool(numOfWorkers)
+
+	go func() {
+		for _, record := range records[1:] { // Skipping header row
+			if job := parseRecord(record); job == nil {
+				continue
+			} else {
+				jobs <- job
+			}
+		}
+		close(jobs)
+	}()
+}
+
+func createWorkerPool(numOfWorkers int) {
+	wg.Add(numOfWorkers)
+	for range numOfWorkers {
+		go worker()
+	}
+	wg.Wait()
+}
+
+func worker() {
+	for job := range jobs {
+		processJob(0, job)
+	}
+	wg.Done()
+}
